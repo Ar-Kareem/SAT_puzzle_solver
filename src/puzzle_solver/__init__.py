b167617d2f2e60b16234cc0532637d173ae1d808
Ar-Kareem <abdulrahman_3000@hotmail.com>
2025-10-20 03:51:03 -0400

    fix images

--- src/puzzle_solver/__init__.py
+++ src/puzzle_solver/__init__.py
@@ -33,4 +33,4 @@ from puzzle_solver.puzzles.unruly import unruly as unruly_solver
 
 from puzzle_solver.puzzles.inertia.parse_map.parse_map import main as inertia_image_parser
 
-__version__ = '0.9.9'
+__version__ = '0.9.10'
