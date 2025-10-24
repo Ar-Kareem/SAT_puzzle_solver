19aaa898fbc1058428f2f92b463212b95ae2b63a
Ar-Kareem <abdulrahman_3000@hotmail.com>
2025-10-24 03:16:07 -0400

    MAJOR SPEEDUP PALISLIDE (from 1.15 hours to 20 seconds to solve 15x12)

--- src/puzzle_solver/puzzles/palisade/palisade.py
+++ src/puzzle_solver/puzzles/palisade/palisade.py
@@ -1,9 +1,52 @@
+from dataclasses import dataclass
+from collections import defaultdict
+
+from tqdm import tqdm
 import numpy as np
 from ortools.sat.python import cp_model
-from ortools.sat.python.cp_model import LinearExpr as lxp
 
-from puzzle_solver.core.utils import Pos, get_all_pos, get_neighbors4, id_board_to_wall_board, render_grid, set_char, get_char, get_neighbors8, in_bounds, get_next_pos, Direction
-from puzzle_solver.core.utils_ortools import generic_solve_all, SingleSolution, force_connected_component
+from puzzle_solver.core.utils import Pos, get_all_pos, get_pos, id_board_to_wall_board, render_grid, set_char, in_bounds, get_next_pos, Direction, polyominoes
+from puzzle_solver.core.utils_ortools import generic_solve_all, SingleSolution
+
+
+
+# a shape on the 2d board is just a set of positions
+Shape = frozenset[Pos]
+
+@dataclass(frozen=True)
+class ShapeOnBoard:
+    is_active: cp_model.IntVar
+    shape: Shape
+    shape_id: int
+    body: set[Pos]
+
+
+def get_valid_translations(shape: Shape, board: np.array) -> set[Pos]:
+    # give a shape and a board, return all valid translations of the shape that are fully contained in the board AND consistent with the clues on the board
+    shape_list = list(shape)
+    shape_borders = []  # will contain the number of borders for each pos in the shape; this has to be consistent with the clues on the board
+    for pos in shape_list:
+        v = 0
+        for direction in Direction:
+            next_pos = get_next_pos(pos, direction)
+            if not in_bounds(next_pos, board.shape[0], board.shape[1]) or next_pos not in shape:
+                v += 1
+        shape_borders.append(v)
+    shape_list = [(p.x, p.y) for p in shape_list]
+    # min x/y is always 0
+    max_x = max(p[0] for p in shape_list)
+    max_y = max(p[1] for p in shape_list)
+
+    for dy in range(0, board.shape[0] - max_y):
+        for dx in range(0, board.shape[1] - max_x):
+            body = tuple((p[0] + dx, p[1] + dy) for p in shape_list)
+            for i, p in enumerate(body):
+                c = board[p[1], p[0]]
+                if c != ' ' and c != str(shape_borders[i]):  # there is a clue and it doesn't match my translated shape, skip
+                    break
+            else:
+                yield tuple(get_pos(x=p[0], y=p[1]) for p in body)
+
 
 
 class Board:
@@ -17,82 +60,40 @@ class Board:
         self.region_count = (self.V * self.H) // self.region_size
         assert self.region_count * self.region_size == self.V * self.H, f'region_size must be a factor of the board size, got {self.region_size} and {self.region_count}'
 
-        self.model = cp_model.CpModel()
-        self.pos_region_indicator: dict[tuple[Pos, int], cp_model.IntVar] = {}
-        self.pos_val: dict[Pos, cp_model.IntVar] = {}
-        self.walls: dict[frozenset[Pos], cp_model.IntVar] = {}
-        self.roots: list[list[cp_model.IntVar]] = []  # for each region, for each pos, 1/0 if this pos is root of this region
+        self.polyominoes = polyominoes(self.region_size)
 
+        self.model = cp_model.CpModel()
+        self.shapes_on_board: list[ShapeOnBoard] = []  # will contain every possible shape on the board based on polyomino degrees
+        self.pos_to_shapes: dict[Pos, set[ShapeOnBoard]] = defaultdict(set)
         self.create_vars()
         self.add_all_constraints()
+        print('Number of variables:', len(self.model.proto.variables))
 
     def create_vars(self):
-        for pos in get_all_pos(self.V, self.H):
-            self.pos_val[pos] = self.model.NewIntVar(0, self.region_count, f'{pos}')
-            for region_idx in range(self.region_count):
-                self.pos_region_indicator[(pos, region_idx)] = self.model.NewBoolVar(f'{pos}:{region_idx}')
-            
-            for direction in [Direction.RIGHT, Direction.DOWN]:
-                next_pos = get_next_pos(pos, direction)
-                if not in_bounds(next_pos, self.V, self.H):
-                    continue
-                self.walls[frozenset((pos, next_pos))] = self.model.NewBoolVar(f'{pos}:{next_pos}')
+        for shape in tqdm(self.polyominoes):
+            for body in get_valid_translations(shape, self.board):
+                uid = len(self.shapes_on_board)
+                shape_on_board = ShapeOnBoard(
+                    is_active=self.model.NewBoolVar(f'{uid}:is_active'),
+                    shape=shape,
+                    shape_id=uid,
+                    body=body,
+                )
+                self.shapes_on_board.append(shape_on_board)
+                for pos in body:
+                    self.pos_to_shapes[pos].add(shape_on_board)
 
     def add_all_constraints(self):
-        self.enforce_regions()  # sets up variables
-        self.enforce_clues()  # enforces clues are correct
-        self.enforce_connected_components()  # forces regions to be connected otherwise solutions will contain disconnected subgraphs that belong to the same region
-        self.break_region_symmetry()  # forces regions to be in order to break symmetry
-    
-    def enforce_regions(self):
-        # each pos belongs to exactly one region
-        for pos in get_all_pos(self.V, self.H):
-            self.model.AddExactlyOne([self.pos_region_indicator[(pos, region_idx)] for region_idx in range(self.region_count)])
-        # region sizes are correct
-        for region_idx in range(self.region_count):
-            region_vars = [self.pos_region_indicator[(pos, region_idx)] for pos in get_all_pos(self.V, self.H)]
-            self.model.Add(lxp.Sum(region_vars) == self.region_size)
-        # setup pos val variables: pos val is equal to the region that is on
-        for pos in get_all_pos(self.V, self.H):
-            for region_idx in range(self.region_count):
-                self.model.Add(self.pos_val[pos] == region_idx).OnlyEnforceIf(self.pos_region_indicator[(pos, region_idx)])
-        # setup walls variable: walls is true if the two positions are different regions
-        for (pos1, pos2), var in self.walls.items():
-            self.model.Add(self.pos_val[pos1] != self.pos_val[pos2]).OnlyEnforceIf(var)
-            self.model.Add(self.pos_val[pos1] == self.pos_val[pos2]).OnlyEnforceIf(var.Not())
-
-    def enforce_clues(self):
-        # the clue is how many neighbours are different than me (a border is an automatic +1)
-        for pos in get_all_pos(self.V, self.H):
-            c = get_char(self.board, pos)
-            if not str(c).isdecimal():
-                continue
-            clue = int(c)
-            wall_vars = [self.walls[frozenset((pos, n))] for n in get_neighbors4(pos, self.V, self.H)]
-            border_count = 4 - len(wall_vars)
-            self.model.Add(lxp.Sum(wall_vars) + border_count == clue)
-
-    def enforce_connected_components(self):
-        # each region is connected
-        for region_idx in range(self.region_count):
-            region_vars = {pos: self.pos_region_indicator[(pos, region_idx)] for pos in get_all_pos(self.V, self.H)}
-            fc_vars = force_connected_component(self.model, region_vars)
-            self.roots.append([fc_vars["is_root"][pos] for pos in get_all_pos(self.V, self.H)])
-
-    def break_region_symmetry(self):
-        assert len(self.roots) == self.region_count, f'expected {self.region_count} roots, got {len(self.roots)} (are you sure self.roots is initialized?)'
-        # enforce strict root order to break symmetry
-        for k in range(self.region_count - 1):
-            lhs = sum(i * self.roots[k][i]     for i in range(self.V * self.H))  # index of root in region k
-            rhs = sum(i * self.roots[k + 1][i] for i in range(self.V * self.H))  # index of root in region k+1
-            self.model.Add(lhs < rhs)  # strictly before
-
+        for pos in get_all_pos(self.V, self.H):  # each position has exactly one shape active
+            self.model.AddExactlyOne(shape.is_active for shape in self.pos_to_shapes[pos])
 
     def solve_and_print(self, verbose: bool = True):
         def board_to_solution(board: Board, solver: cp_model.CpSolverSolutionCallback) -> SingleSolution:
             assignment: dict[Pos, int] = {}
-            for pos, var in board.pos_val.items():
-                assignment[pos] = solver.Value(var)
+            for shape in board.shapes_on_board:
+                if solver.Value(shape.is_active) == 1:
+                    for pos in shape.body:
+                        assignment[pos] = shape.shape_id
             return SingleSolution(assignment=assignment)
         def callback(single_res: SingleSolution):
             print("Solution found")
@@ -104,5 +105,72 @@ class Board:
             for row in id_board:
                 print('       ', row.tolist(), end=',\n')
             print('    ])')
-            print(render_grid(id_board_to_wall_board(id_board), center_char=self.board))
+            board = np.where(self.board == ' ', '·', self.board)
+            print(render_grid(id_board_to_wall_board(id_board), center_char=board))
         return generic_solve_all(self, board_to_solution, callback=callback if verbose else None, verbose=verbose)
+
+
+
+# BEFORE OPTIMIZATION (1.15 hours to solve)
+#     0   0   0   0   0   0   0   0   0   0   1   1   1   1   1
+#     0   1   2   3   4   5   6   7   8   9   0   1   2   3   4
+#   ┌───────────────────┬───────────────────────┬───────────────┐
+#  0│ 2                 │ 3           1   1   3 │               │
+#   │   ┌───────────┐   ├───────┬───┐       ┌───┴───┐           │
+#  1│ 3 │ 2   1     │ 2 │ 3     │   │       │     2 │     0     │
+#   ├───┘           │   └───┐   │   └───┐   └───┐   └───┐       │
+#  2│             1 │ 1     │   │ 1     │       │ 1     │       │
+#   │   ┌───┐       │   ┌───┘   │   ┌───┴───────┘       └───┐   │
+#  3│   │ 3 │ 2     │   │       │ 2 │ 3               1     │   │
+#   ├───┘   └───────┼───┘   ┌───┘   └───┬───────────────┬───┴───┤
+#  4│     0   1     │ 2     │     0     │         1     │ 3   2 │
+#   │           ┌───┘       │           │   ┌───┐       └───┐   │
+#  5│ 1   0     │         2 │ 2       2 │   │ 3 │     0   2 │   │
+#   │       ┌───┴───────────┼───┬───────┴───┤   ├───┐       │   │
+#  6│       │             3 │   │         2 │   │   │       │   │
+#   ├───────┘   ┌───────────┤   └───┐       │   │   └───────┘   │
+#  7│     1     │         3 │ 1     │ 1     │   │         1     │
+#   │       ┌───┘       ┌───┘       │       │   └───────┐   ┌───┤
+#  8│       │     0     │     0     │     1 │ 2         │   │ 3 │
+#   │   ┌───┘       ┌───┤           ├───┐   └───┐       ├───┘   │
+#  9│   │           │   │     1     │   │ 2     │     1 │ 2   1 │
+#   ├───┤   ┌───────┘   ├───────┐   │   └───┐   │       │       │
+# 10│   │   │         1 │     2 │ 3 │ 1     │   │     2 │     1 │
+#   │   └───┘           │       └───┘       ├───┴───────┘       │
+# 11│ 2       1       2 │ 2   1           2 │                   │
+#   └───────────────────┴───────────────────┴───────────────────┘
+# Solutions found: 1
+# status: OPTIMAL
+# Time taken: 4214.79 seconds
+
+# AFTER OPTIMIZATION (11.56 seconds to solve)
+#     0   0   0   0   0   0   0   0   0   0   1   1   1   1   1
+#     0   1   2   3   4   5   6   7   8   9   0   1   2   3   4
+#   ┌───────────────────┬───────────────────────┬───────────────┐
+#  0│ 2   ·   ·   ·   · │ 3   ·   ·   1   1   3 │ ·   ·   ·   · │
+#   │   ┌───────────┐   ├───────┬───┐       ┌───┴───┐           │
+#  1│ 3 │ 2   1   · │ 2 │ 3   · │ · │ ·   · │ ·   2 │ ·   0   · │
+#   ├───┘           │   └───┐   │   └───┐   └───┐   └───┐       │
+#  2│ ·   ·   ·   1 │ 1   · │ · │ 1   · │ ·   · │ 1   · │ ·   · │
+#   │   ┌───┐       │   ┌───┘   │   ┌───┴───────┘       └───┐   │
+#  3│ · │ 3 │ 2   · │ · │ ·   · │ 2 │ 3   ·   ·   ·   1   · │ · │
+#   ├───┘   └───────┼───┘   ┌───┘   └───┬───────────────┬───┴───┤
+#  4│ ·   0   1   · │ 2   · │ ·   0   · │ ·   ·   1   · │ 3   2 │
+#   │           ┌───┘       │           │   ┌───┐       └───┐   │
+#  5│ 1   0   · │ ·   ·   2 │ 2   ·   2 │ · │ 3 │ ·   0   2 │ · │
+#   │       ┌───┴───────────┼───┬───────┴───┤   ├───┐       │   │
+#  6│ ·   · │ ·   ·   ·   3 │ · │ ·   ·   2 │ · │ · │ ·   · │ · │
+#   ├───────┘   ┌───────────┤   └───┐       │   │   └───────┘   │
+#  7│ ·   1   · │ ·   ·   3 │ 1   · │ 1   · │ · │ ·   ·   1   · │
+#   │       ┌───┘       ┌───┘       │       │   └───────┐   ┌───┤
+#  8│ ·   · │ ·   0   · │ ·   0   · │ ·   1 │ 2   ·   · │ · │ 3 │
+#   │   ┌───┘       ┌───┤           ├───┐   └───┐       ├───┘   │
+#  9│ · │ ·   ·   · │ · │ ·   1   · │ · │ 2   · │ ·   1 │ 2   1 │
+#   ├───┤   ┌───────┘   ├───────┐   │   └───┐   │       │       │
+# 10│ · │ · │ ·   ·   1 │ ·   2 │ 3 │ 1   · │ · │ ·   2 │ ·   1 │
+#   │   └───┘           │       └───┘       ├───┴───────┘       │
+# 11│ 2   ·   1   ·   2 │ 2   1   ·   ·   2 │ ·   ·   ·   ·   · │
+#   └───────────────────┴───────────────────┴───────────────────┘
+# Solutions found: 1
+# status: OPTIMAL
+# Time taken: 11.56 seconds
