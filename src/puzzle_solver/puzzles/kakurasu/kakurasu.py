55028a97c1f554ffc0893f7c58a09c175ae48dfe
Ar-Kareem <abdulrahman_3000@hotmail.com>
2025-10-29 18:14:27 -0400

    kakurasu update visualization

--- src/puzzle_solver/puzzles/kakurasu/kakurasu.py
+++ src/puzzle_solver/puzzles/kakurasu/kakurasu.py
@@ -1,22 +1,21 @@
 import numpy as np
 from ortools.sat.python import cp_model
 
-from puzzle_solver.core.utils import Pos, get_all_pos, set_char, get_pos
+from puzzle_solver.core.utils import Pos, get_all_pos, get_pos
 from puzzle_solver.core.utils_ortools import generic_solve_all, SingleSolution
+from puzzle_solver.core.utils_visualizer import combined_function
 
 
 class Board:
     def __init__(self, side: np.array, bottom: np.array):
         assert side.ndim == 1, f'side must be 1d, got {side.ndim}'
-        self.V = side.shape[0]
         assert bottom.ndim == 1, f'bottom must be 1d, got {bottom.ndim}'
+        self.V = side.shape[0]
         self.H = bottom.shape[0]
         self.side = side
         self.bottom = bottom
-
         self.model = cp_model.CpModel()
         self.model_vars: dict[Pos, cp_model.IntVar] = {}
-
         self.create_vars()
         self.add_all_constraints()
 
@@ -32,15 +31,8 @@ class Board:
 
     def solve_and_print(self, verbose: bool = True):
         def board_to_solution(board: Board, solver: cp_model.CpSolverSolutionCallback) -> SingleSolution:
-            assignment: dict[Pos, int] = {}
-            for pos, var in board.model_vars.items():
-                assignment[pos] = solver.value(var)
-            return SingleSolution(assignment=assignment)
+            return SingleSolution(assignment={pos: solver.Value(var) for pos, var in board.model_vars.items()})
         def callback(single_res: SingleSolution):
             print("Solution found")
-            res = np.full((self.V, self.H), ' ', dtype=object)
-            for pos in get_all_pos(self.V, self.H):
-                c = 'X' if single_res.assignment[pos] else ' '
-                set_char(res, pos, c)
-            print(res)
+            print(combined_function(self.V, self.H, is_shaded=lambda r, c: single_res.assignment[get_pos(x=c, y=r)]))
         return generic_solve_all(self, board_to_solution, callback=callback if verbose else None, verbose=verbose)
