83e596b33dd3822e6ffe822e88a83bbc0b1e5c79
Ar-Kareem <abdulrahman_3000@hotmail.com>
2025-10-30 03:06:59 -0400

    filling update visualization

--- src/puzzle_solver/puzzles/filling/filling.py
+++ src/puzzle_solver/puzzles/filling/filling.py
@@ -3,8 +3,9 @@ from dataclasses import dataclass
 import numpy as np
 from ortools.sat.python import cp_model
 
-from puzzle_solver.core.utils import Pos, get_all_pos, get_char, set_char, polyominoes, in_bounds, get_next_pos, Direction
+from puzzle_solver.core.utils import Pos, get_all_pos, get_char, get_pos, polyominoes, in_bounds, get_next_pos, Direction
 from puzzle_solver.core.utils_ortools import generic_solve_all, SingleSolution
+from puzzle_solver.core.utils_visualizer import combined_function, id_board_to_wall_fn
 
 
 @dataclass
@@ -23,8 +24,6 @@ class Board:
         assert all((c == ' ') or (str(c).isdecimal() and 0 <= int(c) <= 9) for c in np.nditer(board)), "board must contain space or digits 0..9"
         self.digits = digits
         self.polyominoes = {d: polyominoes(d) for d in self.digits}
-        # len_shapes = sum(len(shapes) for shapes in self.polyominoes.values())
-        # print(f'total number of shapes: {len_shapes}')
 
         self.model = cp_model.CpModel()
         self.model_vars: dict[Pos, cp_model.IntVar] = {}
@@ -33,7 +32,7 @@ class Board:
         self.forced_pos: dict[Pos, int] = {}
 
         self.create_vars()
-        self.constrain_numbers_on_board()
+        self.force_hints()
         self.init_polyominoes_on_board()
         self.add_all_constraints()
 
@@ -42,8 +41,7 @@ class Board:
             for d in self.digits:
                 self.model_vars[(d,pos)] = self.model.NewBoolVar(f'{d}:{pos}')
 
-    def constrain_numbers_on_board(self):
-        # force numbers already on the board
+    def force_hints(self):
         for pos in get_all_pos(self.V, self.H):
             c = get_char(self.board, pos)
             if c.isdecimal():
@@ -51,7 +49,6 @@ class Board:
                 self.forced_pos[pos] = int(c)
 
     def init_polyominoes_on_board(self):
-        # total_count = 0
         for d in self.digits:  # all digits
             digit_count = 0
             for pos in get_all_pos(self.V, self.H):  # translate by shape
@@ -73,45 +70,25 @@ class Board:
                     for p in body:
                         self.body_loc_to_shape[(d,p)].append(shape_on_board)
                     digit_count += 1
-        #             total_count += 1
-        #             if total_count % 1000 == 0:
-        #                 print(f'{total_count} shapes on board')
-        # print(f'total number of shapes on board: {total_count}')
 
     def add_all_constraints(self):
         for pos in get_all_pos(self.V, self.H):
-            # exactly one digit is active at every position
-            self.model.AddExactlyOne(self.model_vars[(d,pos)] for d in self.digits)
-            # exactly one shape is active at that position
-            self.model.AddExactlyOne(s.is_active for d in self.digits for s in self.body_loc_to_shape[(d,pos)])
-        # if a shape is active then all its body is active
-
-        for s_list in self.body_loc_to_shape.values():
+            self.model.AddExactlyOne(self.model_vars[(d,pos)] for d in self.digits)  # exactly one digit is active at every position
+            self.model.AddExactlyOne(s.is_active for d in self.digits for s in self.body_loc_to_shape[(d,pos)])  # exactly one shape is active at that position
+        for s_list in self.body_loc_to_shape.values():  # if a shape is active then all its body is active
             for s in s_list:
                 for p in s.body:
                     self.model.Add(self.model_vars[(s.N,p)] == 1).OnlyEnforceIf(s.is_active)
-
-        # same shape cannot touch
-        for d, s_list in self.digit_to_shapes.items():
+        for d, s_list in self.digit_to_shapes.items():  # same shape cannot touch each other
             for s in s_list:
                 for disallow_pos in s.disallow_same_shape:
                     self.model.Add(self.model_vars[(d,disallow_pos)] == 0).OnlyEnforceIf(s.is_active)
 
     def solve_and_print(self, verbose: bool = True):
         def board_to_solution(board: "Board", solver: cp_model.CpSolverSolutionCallback) -> SingleSolution:
-            assignment: dict[Pos, int] = {}
-            for pos in get_all_pos(self.V, self.H):
-                for d in self.digits:
-                    if solver.Value(self.model_vars[(d,pos)]) == 1:
-                        assignment[pos] = d
-                        break
-            return SingleSolution(assignment=assignment)
+            return SingleSolution(assignment={pos: d for pos in get_all_pos(self.V, self.H) for d in self.digits if solver.Value(self.model_vars[(d,pos)]) == 1})
         def callback(single_res: SingleSolution):
             print("Solution found")
-            res = np.full((self.V, self.H), ' ', dtype=object)
-            for pos in get_all_pos(self.V, self.H):
-                c = get_char(self.board, pos)
-                c = single_res.assignment[pos]
-                set_char(res, pos, c)
-            print(res)
+            res_arr = np.array([[single_res.assignment[get_pos(x=c, y=r)] for c in range(self.H)] for r in range(self.V)])
+            print(combined_function(self.V, self.H, cell_flags=id_board_to_wall_fn(res_arr), center_char=lambda r, c: res_arr[r, c]))
         return generic_solve_all(self, board_to_solution, callback=callback if verbose else None, verbose=verbose)
