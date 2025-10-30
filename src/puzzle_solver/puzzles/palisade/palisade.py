bd4a5ec285cfe5e2713e286bfd14342d4a4a06e9
Ar-Kareem <abdulrahman_3000@hotmail.com>
2025-10-30 19:59:22 -0400

    palisade refactors

--- src/puzzle_solver/puzzles/palisade/palisade.py
+++ src/puzzle_solver/puzzles/palisade/palisade.py
@@ -9,9 +9,7 @@ from puzzle_solver.core.utils_ortools import generic_solve_all, SingleSolution
 from puzzle_solver.core.utils_visualizer import combined_function, id_board_to_wall_fn
 
 
-
-# a shape on the 2d board is just a set of positions
-Shape = frozenset[Pos]
+Shape = frozenset[Pos]  # a shape on the 2d board is just a set of positions
 
 @dataclass(frozen=True)
 class ShapeOnBoard:
@@ -36,7 +34,6 @@ def get_valid_translations(shape: Shape, board: np.array) -> set[Pos]:
     # min x/y is always 0
     max_x = max(p[0] for p in shape_list)
     max_y = max(p[1] for p in shape_list)
-
     for dy in range(0, board.shape[0] - max_y):
         for dx in range(0, board.shape[1] - max_x):
             body = tuple((p[0] + dx, p[1] + dy) for p in shape_list)
@@ -46,7 +43,6 @@ def get_valid_translations(shape: Shape, board: np.array) -> set[Pos]:
                     break
             else:
                 yield frozenset(get_pos(x=p[0], y=p[1]) for p in body)
-
 
 
 class Board:
@@ -59,7 +55,6 @@ class Board:
         self.region_size = region_size
         self.region_count = (self.V * self.H) // self.region_size
         assert self.region_count * self.region_size == self.V * self.H, f'region_size must be a factor of the board size, got {self.region_size} and {self.region_count}'
-
         self.polyominoes = polyominoes(self.region_size)
 
         self.model = cp_model.CpModel()
@@ -74,9 +69,7 @@ class Board:
                 uid = len(self.shapes_on_board)
                 shape_on_board = ShapeOnBoard(
                     is_active=self.model.NewBoolVar(f'{uid}:is_active'),
-                    shape=shape,
-                    shape_id=uid,
-                    body=body,
+                    shape=shape, shape_id=uid, body=body
                 )
                 self.shapes_on_board.append(shape_on_board)
                 for pos in body:
@@ -88,19 +81,11 @@ class Board:
 
     def solve_and_print(self, verbose: bool = True):
         def board_to_solution(board: Board, solver: cp_model.CpSolverSolutionCallback) -> SingleSolution:
-            assignment: dict[Pos, int] = {}
-            for shape in board.shapes_on_board:
-                if solver.Value(shape.is_active) == 1:
-                    for pos in shape.body:
-                        assignment[pos] = shape.shape_id
-            return SingleSolution(assignment=assignment)
+            active_shapes = [shape for shape in board.shapes_on_board if solver.Value(shape.is_active) == 1]
+            return SingleSolution(assignment={pos: shape.shape_id for shape in active_shapes for pos in shape.body})
         def callback(single_res: SingleSolution):
             print("Solution found")
-            id_board = np.full((self.V, self.H), ' ', dtype=object)
-            for pos in get_all_pos(self.V, self.H):
-                region_idx = single_res.assignment[pos]
-                set_char(id_board, pos, region_idx)
             print(combined_function(self.V, self.H,
-                cell_flags=id_board_to_wall_fn(id_board),
+                cell_flags=id_board_to_wall_fn(np.array([[single_res.assignment[get_pos(x=c, y=r)] for c in range(self.H)] for r in range(self.V)])),
                 center_char=lambda r, c: self.board[r, c] if self.board[r, c] != ' ' else '·'))
         return generic_solve_all(self, board_to_solution, callback=callback if verbose else None, verbose=verbose)
