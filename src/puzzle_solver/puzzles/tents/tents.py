40b4dfe2a140bffc861418d5b1b6725239bcf8d9
Ar-Kareem <abdulrahman_3000@hotmail.com>
2025-10-30 04:48:45 -0400

    tents update visualization

--- src/puzzle_solver/puzzles/tents/tents.py
+++ src/puzzle_solver/puzzles/tents/tents.py
@@ -4,107 +4,77 @@ import numpy as np
 from ortools.sat.python import cp_model
 from ortools.sat.python.cp_model import LinearExpr as lxp
 
-from puzzle_solver.core.utils import Pos, get_all_pos, get_char, set_char, get_neighbors8, get_next_pos, Direction, get_row_pos, get_col_pos
+from puzzle_solver.core.utils import Direction, Pos, get_all_pos, get_char, get_neighbors8, get_next_pos, get_row_pos, get_col_pos, get_opposite_direction, get_pos
 from puzzle_solver.core.utils_ortools import generic_solve_all, SingleSolution
+from puzzle_solver.core.utils_visualizer import combined_function
 
 
 class Board:
-    def __init__(self, board: np.array, sides: dict[str, np.array]):
+    def __init__(self, board: np.array, side: np.array, top: np.array):
         assert board.ndim == 2, f'board must be 2d, got {board.ndim}'
-        assert board.shape[0] == board.shape[1], 'board must be square'
-        assert len(sides) == 2, '2 sides must be provided'
-        assert set(sides.keys()) == set(['top', 'side'])
-        assert all(s.ndim == 1 and s.shape[0] == board.shape[0] for s in sides.values()), 'all sides must be equal to board size'
+        assert side.ndim == 1 and side.shape[0] == board.shape[0], 'side must be 1d and equal to board size'
+        assert top.ndim == 1 and top.shape[0] == board.shape[1], 'top must be 1d and equal to board size'
         assert all(c.item() in [' ', 'T'] for c in np.nditer(board)), 'board must contain only space or T'
         self.board = board
-        self.N = board.shape[0]
-        self.star_positions: set[Pos] = {pos for pos in get_all_pos(self.N) if get_char(self.board, pos) == ' '}
-        self.tree_positions: set[Pos] = {pos for pos in get_all_pos(self.N) if get_char(self.board, pos) == 'T'}
+        self.V, self.H = board.shape
+        self.side = side
+        self.top = top
+        self.non_tree_positions: set[Pos] = {pos for pos in get_all_pos(self.V, self.H) if get_char(self.board, pos) == ' '}
+        self.tree_positions: set[Pos] = {pos for pos in get_all_pos(self.V, self.H) if get_char(self.board, pos) == 'T'}
+
         self.model = cp_model.CpModel()
-        self.is_tent = defaultdict(int)
-        self.tent_direction = defaultdict(int)
-        self.sides = sides
+        self.is_tent: dict[Pos, cp_model.IntVar] = defaultdict(int)
+        self.tent_direction: dict[tuple[Pos, Direction], cp_model.IntVar] = defaultdict(int)
         self.create_vars()
         self.add_all_constraints()
 
     def create_vars(self):
-        for pos in self.star_positions:
-            is_tent = self.model.NewBoolVar(f'{pos}:is_tent')
-            tent_direction = self.model.NewIntVar(0, 4, f'{pos}:tent_direction')
-            self.model.Add(tent_direction == 0).OnlyEnforceIf(is_tent.Not())
-            self.model.Add(tent_direction > 0).OnlyEnforceIf(is_tent)
-            self.is_tent[pos] = is_tent
-            self.tent_direction[pos] = tent_direction
+        for pos in self.non_tree_positions:
+            self.is_tent[pos] = self.model.NewBoolVar(f'{pos}:is_tent')
+        for pos in self.tree_positions:
+            for direction in Direction:
+                tent_pos = get_next_pos(pos, direction)
+                if tent_pos not in self.is_tent:
+                    continue
+                opposite_direction = get_opposite_direction(direction)
+                tent_direction = self.model.NewBoolVar(f'{pos}:{direction}')
+                self.model.Add(tent_direction == 0).OnlyEnforceIf(self.is_tent[tent_pos].Not())
+                self.tent_direction[(pos, direction)] = tent_direction
+                self.tent_direction[(tent_pos, opposite_direction)] = tent_direction
 
     def add_all_constraints(self):
         # - There are exactly as many tents as trees.
-        self.model.Add(lxp.sum([self.is_tent[pos] for pos in self.star_positions]) == len(self.tree_positions))
+        self.model.Add(lxp.sum([self.is_tent[pos] for pos in self.non_tree_positions]) == len(self.tree_positions))
         # - no two tents are adjacent horizontally, vertically or diagonally
-        for pos in self.star_positions:
-            for neighbour in get_neighbors8(pos, V=self.N, H=self.N, include_self=False):
-                if get_char(self.board, neighbour) != ' ':
-                    continue
+        for pos in self.non_tree_positions:
+            for neighbour in get_neighbors8(pos, V=self.V, H=self.H, include_self=False):
                 self.model.Add(self.is_tent[neighbour] == 0).OnlyEnforceIf(self.is_tent[pos])
         # - the number of tents in each row and column matches the numbers around the edge of the grid
-        for row in range(self.N):
-            row_vars = [self.is_tent[pos] for pos in get_row_pos(row, self.N)]
-            self.model.Add(lxp.sum(row_vars) == self.sides['side'][row])
-        for col in range(self.N):
-            col_vars = [self.is_tent[pos] for pos in get_col_pos(col, self.N)]
-            self.model.Add(lxp.sum(col_vars) == self.sides['top'][col])
+        for row in range(self.V):
+            if self.side[row] == -1:
+                continue
+            row_vars = [self.is_tent[pos] for pos in get_row_pos(row, H=self.H)]
+            self.model.Add(lxp.sum(row_vars) == self.side[row])
+        for col in range(self.H):
+            if self.top[col] == -1:
+                continue
+            col_vars = [self.is_tent[pos] for pos in get_col_pos(col, V=self.V)]
+            self.model.Add(lxp.sum(col_vars) == self.top[col])
         # - it is possible to match tents to trees so that each tree is orthogonally adjacent to its own tent (but may also be adjacent to other tents).
-        # for each tree, one of the following must be true:
-        # a tent on its left has direction RIGHT
-        # a tent on its right has direction LEFT
-        # a tent on its top has direction DOWN
-        # a tent on its bottom has direction UP
-        for tree in self.tree_positions:
-            self.add_tree_constraints(tree)
-
-    def add_tree_constraints(self, tree_pos: Pos):
-        left_pos = get_next_pos(tree_pos, Direction.LEFT)
-        right_pos = get_next_pos(tree_pos, Direction.RIGHT)
-        top_pos = get_next_pos(tree_pos, Direction.UP)
-        bottom_pos = get_next_pos(tree_pos, Direction.DOWN)
-        var_list = []
-        if left_pos in self.star_positions:
-            aux = self.model.NewBoolVar(f'{tree_pos}:left')
-            self.model.Add(self.tent_direction[left_pos] == Direction.RIGHT.value).OnlyEnforceIf(aux)
-            self.model.Add(self.tent_direction[left_pos] != Direction.RIGHT.value).OnlyEnforceIf(aux.Not())
-            var_list.append(aux)
-        if right_pos in self.star_positions:
-            aux = self.model.NewBoolVar(f'{tree_pos}:right')
-            self.model.Add(self.tent_direction[right_pos] == Direction.LEFT.value).OnlyEnforceIf(aux)
-            self.model.Add(self.tent_direction[right_pos] != Direction.LEFT.value).OnlyEnforceIf(aux.Not())
-            var_list.append(aux)
-        if top_pos in self.star_positions:
-            aux = self.model.NewBoolVar(f'{tree_pos}:top')
-            self.model.Add(self.tent_direction[top_pos] == Direction.DOWN.value).OnlyEnforceIf(aux)
-            self.model.Add(self.tent_direction[top_pos] != Direction.DOWN.value).OnlyEnforceIf(aux.Not())
-            var_list.append(aux)
-        if bottom_pos in self.star_positions:
-            aux = self.model.NewBoolVar(f'{tree_pos}:bottom')
-            self.model.Add(self.tent_direction[bottom_pos] == Direction.UP.value).OnlyEnforceIf(aux)
-            self.model.Add(self.tent_direction[bottom_pos] != Direction.UP.value).OnlyEnforceIf(aux.Not())
-            var_list.append(aux)
-        self.model.AddBoolOr(var_list)
+        # each tent is pointing exactly once at a tree
+        for pos in self.non_tree_positions:
+            var_list = [self.tent_direction[(pos, direction)] for direction in Direction]
+            self.model.Add(lxp.sum(var_list) == 1).OnlyEnforceIf(self.is_tent[pos])
+            self.model.Add(lxp.sum(var_list) == 0).OnlyEnforceIf(self.is_tent[pos].Not())
+        # each tree is pointed at by exactly one tent
+        for pos in self.tree_positions:
+            var_list = [self.tent_direction[(pos, direction)] for direction in Direction]
+            self.model.Add(lxp.sum(var_list) == 1)
 
     def solve_and_print(self, verbose: bool = True):
         def board_to_solution(board: Board, solver: cp_model.CpSolverSolutionCallback) -> SingleSolution:
-            assignment: dict[Pos, int] = {}
-            for pos, var in board.is_tent.items():
-                if isinstance(var, int):
-                    continue
-                assignment[pos] = solver.value(var)
-            return SingleSolution(assignment=assignment)
+            return SingleSolution(assignment={pos: solver.value(var) for pos, var in board.is_tent.items() if not isinstance(var, int)})
         def callback(single_res: SingleSolution):
             print("Solution found")
-            res = np.full((self.N, self.N), ' ', dtype=object)
-            for pos in get_all_pos(self.N):
-                c = get_char(self.board, pos)
-                if c == ' ':
-                    c = single_res.assignment[pos]
-                    c = 'E' if c == 1 else ' '
-                set_char(res, pos, c)
-            print(res)
-        return generic_solve_all(self, board_to_solution, callback=callback if verbose else None, verbose=verbose)
+            print(combined_function(self.V, self.H, center_char=lambda r, c: ('|' if self.board[r][c].strip() else ('▲' if single_res.assignment[get_pos(c, r)] == 1 else ' '))))
+        return generic_solve_all(self, board_to_solution, callback=callback if verbose else None, verbose=verbose, max_solutions=5)
--- tests/test_tents.py
+++ tests/test_tents.py
@@ -26,7 +26,7 @@ def test_ground():
   side = np.array([4, 1, 6, 0, 5, 2, 3, 1, 5, 2, 3, 2, 4, 3, 4])
   top = np.array([4, 2, 4, 1, 3, 3, 3, 3, 3, 3, 2, 2, 6, 2, 4])
 
-  binst = solver.Board(board=board, sides={'top': top, 'side': side})
+  binst = solver.Board(board=board, top=top, side=side)
   solutions = binst.solve_and_print()
   ground = np.array([
     [' ', 'T', 'E', ' ', ' ', ' ', ' ', 'E', 'T', ' ', 'T', 'E', 'T', 'E', ' '],
@@ -53,5 +53,32 @@ def test_ground():
     assert solution[pos] == ground_assignment[pos], f'solution[{pos}] != ground_assignment[{pos}], {solution[pos]} != {ground_assignment[pos]}'
 
 
+def test_dummy():
+  board = np.array([
+    [' ', ' ', ' '],
+    [' ', ' ', 'T'],
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', 'T'],
+  ])
+  top = np.array([0, 0, 2])
+  side = np.array([-1, -1, -1, -1, -1])
+  binst = solver.Board(board=board, top=top, side=side)
+  solutions = binst.solve_and_print()
+  ground = np.array([
+    [' ', ' ', 'E'],
+    [' ', ' ', 'T'],
+    [' ', ' ', ' '],
+    [' ', ' ', 'E'],
+    [' ', ' ', 'T'],
+  ])
+  assert len(solutions) == 1, f'unique solutions != 1, == {len(solutions)}'
+  solution = solutions[0].assignment
+  ground_assignment = {get_pos(x=x, y=y): 1 if ground[y][x] == 'E' else 0 for x in range(ground.shape[1]) for y in range(ground.shape[0]) if ground[y][x] in [' ', 'E']}
+  assert set(solution.keys()) == set(ground_assignment.keys()), f'solution keys != ground assignment keys, {set(solution.keys()) ^ set(ground_assignment.keys())} \n\n\n{solution} \n\n\n{ground_assignment}'
+  for pos in solution.keys():
+    assert solution[pos] == ground_assignment[pos], f'solution[{pos}] != ground_assignment[{pos}], {solution[pos]} != {ground_assignment[pos]}'
+
 if __name__ == '__main__':
+  test_dummy()
   test_ground()
